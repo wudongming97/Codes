eb35cca38f3dff4502daad690c3e25fdf5ee8a26
yx <yxue3357@163.com>
2017-12-29 17:50:46 +0800

    add ignore file

--- CVs/VAE_katong_main.py
+++ CVs/VAE_katong_main.py
@@ -16,7 +16,7 @@ flags.DEFINE_integer('steps', 20000, '')
 flags.DEFINE_integer('z_size', 32, '')
 flags.DEFINE_integer('batch_size', 32, '')
 flags.DEFINE_float('lr', 0.0005, '')
-flags.DEFINE_float('beta', 0.0, '')
+flags.DEFINE_float('beta', 1.0, '')
 
 flags.DEFINE_string('images_path', 'E:\\data\\katong\\', '')
 flags.DEFINE_string('file_name', './data/katong.tfrecords', '')

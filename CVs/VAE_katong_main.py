aeaf11ab95efff7c3c496f3758ce7af821f31bfe
yx <yxue3357@163.com>
2017-12-29 17:01:16 +0800

    VAE生成卡通图像 代码V1

---
+++ CVs/VAE_katong_main.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import tensorflow as tf
+
+import utils.Utils as U
+from models.VAE_katong import VAE
+
+flags = tf.app.flags
+flags.DEFINE_string('model_name', 'VAE_katong', '')
+flags.DEFINE_integer('steps', 20000, '')
+flags.DEFINE_integer('z_size', 32, '')
+flags.DEFINE_integer('batch_size', 32, '')
+flags.DEFINE_float('lr', 0.0005, '')
+flags.DEFINE_float('beta', 0.0, '')
+
+flags.DEFINE_string('images_path', 'E:\\data\\katong\\', '')
+flags.DEFINE_string('file_name', './data/katong.tfrecords', '')
+
+flags.DEFINE_string('ckpt_path', './results/VAE_katong/ckpt/', '')
+flags.DEFINE_string('logs_path', './results/VAE_katong/logs/', '')
+FLAGS = flags.FLAGS
+
+
+def main(_):
+    tf.logging.set_verbosity(tf.logging.INFO)
+    # gpu memory
+    sess_conf = tf.ConfigProto(
+        # log_device_placement=True,
+        gpu_options=tf.GPUOptions(
+            allow_growth=True,
+            per_process_gpu_memory_fraction=0.6,
+        )
+    )
+
+    # 写tfrecord文件
+    if not os.path.exists(FLAGS.file_name):
+        U.fixed_image_writer(FLAGS.images_path, FLAGS.file_name)
+
+    graph = tf.Graph()
+    with graph.as_default():
+
+        with tf.name_scope('fixed_image_reader'):
+            images = U.fixed_image_reader([FLAGS.file_name], [96, 96, 3])
+            batch_images = tf.train.shuffle_batch([images],
+                                                  batch_size=FLAGS.batch_size,
+                                                  capacity=2000,
+                                                  min_after_dequeue=100)
+        model = VAE(FLAGS, batch_images)
+
+        saver = tf.train.Saver(  # max_to_keep=5,
+            keep_checkpoint_every_n_hours=1,
+            pad_step_number=True)
+
+    with tf.Session(graph=graph, config=sess_conf) as sess:
+        train_writer = tf.summary.FileWriter(FLAGS.logs_path, sess.graph)
+        sess.run(tf.global_variables_initializer())
+
+        tf.train.export_meta_graph(FLAGS.ckpt_path + FLAGS.model_name + '.meta')
+        ckpt = tf.train.get_checkpoint_state(FLAGS.ckpt_path)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+
+        print('\nbegin fit ...')
+        model.fit(sess, train_writer, saver)
+
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ CVs/utils/Utils.py
@@ -0,0 +1,56 @@
+import os
+
+import tensorflow as tf
+from matplotlib.image import imread
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+# 获取目录下所有以suffix为后缀的文件名
+def get_file_name(dir_, suffix=None):
+    return [os.path.join(dir_, name) for name in os.listdir(dir_)
+            if os.path.isfile(os.path.join(dir_, name)) and name.endswith(suffix)]
+
+
+def fixed_image_writer(images_path, file_name):
+    writer = tf.python_io.TFRecordWriter(file_name)
+    for path_ in get_file_name(images_path):
+        image = imread(path_)
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'image': _bytes_feature(image.tostring())
+        }))
+        writer.write(example.SerializeToString())
+    writer.close()
+
+
+def fixed_image_reader(filename_list, shape_=None):
+    queue_ = tf.train.string_input_producer(filename_list)
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(queue_)
+    # 解析
+    features = tf.parse_single_example(
+        serialized_example,
+        features={
+            'image': tf.FixedLenFeature([], tf.string)
+        }
+    )
+    image = tf.decode_raw(features['image'], tf.uint8)
+    image = tf.cast(image, tf.float32) * (1. / 255)
+    # 将图片转换成多维数组形式
+    image = tf.reshape(image, shape_)
+    image.set_shape(shape_)
+    return image
+
+
+def train_is_ok(sess, steps):
+    return sess.run(tf.train.get_global_step()) >= steps
+
+
+def print_shape(v):
+    print(v.get_shape().as_list())

9bd1ce603ece592e19ce4e96161fb0a129cce66d
yx <yxue3357@163.com>
2018-01-01 16:18:37 +0800

    clean

--- CVs/configs/VAE_cifar10_config.py
+++ CVs/configs/VAE_cifar10_config.py
@@ -9,11 +9,11 @@ flags.DEFINE_float('beta', 1.0, '')
 flags.DEFINE_float('alpha', 0.0, 'the frac of aux_loss')
 flags.DEFINE_float('gamma', 5.0, '')
 
-# ================== VAE_katong =======================
+# ================== VAE_cifar10 =======================
 flags.DEFINE_string('model_name', 'VAE_cifa10', '')
 flags.DEFINE_string('file_name', './data/cifar10_0.tfrecords', '')
 flags.DEFINE_string('ckpt_path', './results/VAE_cifa10/ckpt/', '')
 flags.DEFINE_string('logs_path', './results/VAE_cifa10/logs/', '')
-# ================= VAE_katong ========================
+# ================= VAE_cifar10 ========================
 FLAGS = flags.FLAGS
 SHAPE = [32, 32, 3]

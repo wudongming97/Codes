0a04312fd3989b08d437faa373378b2cd9c0c701
yx <yxue3357@163.com>
2018-03-12 18:51:08 +0800

    vae_cifar10_0

---
+++ CVs/VAE_cifar10/Utils.py
@@ -0,0 +1,114 @@
+import os
+import pickle
+
+import numpy as np
+import tensorflow as tf
+from matplotlib.image import imread
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+# 获取目录下所有以suffix为后缀的文件名
+def get_file_name_by_suffix(dir_, suffix_):
+    return [os.path.join(dir_, name) for name in os.listdir(dir_)
+            if os.path.isfile(os.path.join(dir_, name)) and name.endswith(suffix_)]
+
+
+def get_file_name_by_prefix(dir_, prefix_):
+    return [os.path.join(dir_, name) for name in os.listdir(dir_)
+            if os.path.isfile(os.path.join(dir_, name)) and name.startswith(prefix_)]
+
+
+def get_file_name(dir_):
+    return [os.path.join(dir_, name) for name in os.listdir(dir_)
+            if os.path.isfile(os.path.join(dir_, name))]
+
+
+def single_image_writer(writer, image):
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'image': _bytes_feature(image.tostring())
+    }))
+    writer.write(example.SerializeToString())
+
+
+def fixed_image_writer(filename_list, target_file):
+    writer = tf.python_io.TFRecordWriter(target_file)
+    for path_ in filename_list:
+        image = imread(path_)
+        single_image_writer(writer, image)
+    writer.close()
+
+
+def fixed_image_reader(filename_list, shape_):
+    queue_ = tf.train.string_input_producer(filename_list)
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(queue_)
+    # 解析
+    features = tf.parse_single_example(
+        serialized_example,
+        features={
+            'image': tf.FixedLenFeature([], tf.string)
+        }
+    )
+    image = tf.decode_raw(features['image'], tf.uint8)
+    image = tf.cast(image, tf.float32) * (1. / 255)
+    # 将图片转换成多维数组形式
+    image = tf.reshape(image, shape_)
+    image.set_shape(shape_)
+    return image
+
+
+def train_is_ok(sess, steps):
+    return sess.run(tf.train.get_global_step()) >= steps
+
+
+def print_shape(v):
+    print(v.get_shape().as_list())
+
+
+def cifar10_unpickle(file):
+    with open(file, 'rb') as fo:
+        dick = pickle.load(fo, encoding='bytes')
+    return dick
+
+
+def cifar10_tfrecord_writer(dir_, target, cls=None):
+    from os.path import join
+    writer = tf.python_io.TFRecordWriter(target)
+    data_names = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+    # label_names = cifar10_unpickle(join(dir_, 'batches.meta'))[b'label_names']
+    files_ = [join(dir_, fi) for fi in data_names]
+    for file in files_:
+        dick = cifar10_unpickle(file)
+        data, lables, filenames = dick[b'data'], dick[b'labels'], dick[b'filenames']
+        for ix, l_ in enumerate(lables):
+            image = np.reshape(np.reshape(data[ix], [3, 1024]).T, [32, 32, 3])
+            if cls is None:
+                single_image_writer(writer, image)
+            elif l_ in cls:
+                single_image_writer(writer, image)
+            else:
+                None
+
+    writer.close()
+
+def mnist_tfrecord_writer(dir_, target):
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets(dir_, dtype=tf.uint8)
+    writer = tf.python_io.TFRecordWriter(target)
+    for i in range(mnist.train.num_examples):
+        image = mnist.train.images[i]
+        single_image_writer(writer, image)
+    writer.close()
+
+
+if __name__ == '__main__':
+    # filename_list = get_file_name_by_prefix('./raw', '0_')
+    # fixed_image_writer(filename_list, 'cifar10_0.tfrecords')
+    cifar10_tfrecord_writer('/home/yx/Datasets/CIFAR10', 'cifar10_0.tfrecords', cls=[0])
---
+++ CVs/VAE_cifar10/VAE_cifar10_main.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+import Utils as U
+from VAE_cifar10 import VAE_cifar10
+
+flags = tf.app.flags
+flags.DEFINE_integer('steps', 200000, '')
+flags.DEFINE_integer('z_size', 20, '')
+flags.DEFINE_integer('batch_size', 60, '')
+flags.DEFINE_float('lr', 0.0005, '')
+flags.DEFINE_float('beta', 1.0, '')
+
+# ================== VAE_mnist =======================
+flags.DEFINE_string('model_name', 'VAE_cifar10', '')
+flags.DEFINE_string('file_name', './cifar10.tfrecords', '')
+flags.DEFINE_string('ckpt_path', './results/VAE_cifar10/ckpt/', '')
+flags.DEFINE_string('logs_path', './results/VAE_cifar10/logs/', '')
+# ================= VAE_mnist ========================
+FLAGS = flags.FLAGS
+SHAPE = [28, 28, 1]
+
+
+def main(_):
+    tf.logging.set_verbosity(tf.logging.INFO)
+    sess_conf = tf.ConfigProto(
+        # log_device_placement=True,
+        gpu_options=tf.GPUOptions(
+            allow_growth=True,
+            per_process_gpu_memory_fraction=0.6,
+        )
+    )
+
+    graph = tf.Graph()
+    with graph.as_default():
+        with tf.name_scope('fixed_image_reader'):
+            images = U.fixed_image_reader([FLAGS.file_name], SHAPE)
+            batch_images = tf.train.shuffle_batch([images],
+                                                  batch_size=FLAGS.batch_size,
+                                                  capacity=2000,
+                                                  min_after_dequeue=100)
+        model = VAE_cifar10(FLAGS, batch_images)
+
+        saver = tf.train.Saver(
+            keep_checkpoint_every_n_hours=1,
+            pad_step_number=True)
+
+    with tf.Session(graph=graph, config=sess_conf) as sess:
+        _writer = tf.summary.FileWriter(FLAGS.logs_path, sess.graph)
+        sess.run(tf.global_variables_initializer())
+
+        tf.train.export_meta_graph(FLAGS.ckpt_path + FLAGS.model_name + '.meta')
+        ckpt = tf.train.get_checkpoint_state(FLAGS.ckpt_path)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+
+        print('\nbegin fit ...')
+        if not model.train_is_ok(sess):
+            model.fit(sess, _writer, saver)
+
+
+if __name__ == "__main__":
+    tf.app.run()

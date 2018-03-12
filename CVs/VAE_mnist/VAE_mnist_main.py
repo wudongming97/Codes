f8647f94d0b18ba0cd3e5a001f3cbe572329940e
yx <yxue3357@163.com>
2018-03-12 18:38:20 +0800

    clean and refine

--- CVs/VAE_mnist/VAE_mnist_main.py
+++ CVs/VAE_mnist/VAE_mnist_main.py
@@ -6,14 +6,23 @@ from __future__ import print_function
 import tensorflow as tf
 
 import Utils as U
-
-# ======== cifar10 =========
-# from configs.VAE_cifar10_config import FLAGS, SHAPE
-# from models.VAE_cifar10 import VAE_
-
-#======== mnist ============
-from VAE_mnist_config import FLAGS, SHAPE
 from VAE_mnist import VAE_mnist
+
+flags = tf.app.flags
+flags.DEFINE_integer('steps', 200000, '')
+flags.DEFINE_integer('z_size', 20, '')
+flags.DEFINE_integer('batch_size', 60, '')
+flags.DEFINE_float('lr', 0.0005, '')
+flags.DEFINE_float('beta', 1.0, '')
+
+# ================== VAE_mnist =======================
+flags.DEFINE_string('model_name', 'VAE_mnist', '')
+flags.DEFINE_string('file_name', './mnist.tfrecords', '')
+flags.DEFINE_string('ckpt_path', './results/VAE_mnist/ckpt/', '')
+flags.DEFINE_string('logs_path', './results/VAE_mnist/logs/', '')
+# ================= VAE_mnist ========================
+FLAGS = flags.FLAGS
+SHAPE = [28, 28, 1]
 
 
 def main(_):

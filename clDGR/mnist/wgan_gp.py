06d46b3c9fb19a5696a7d353f517662f4e840c39
yx <yxue3357@163.com>
2018-03-27 10:21:36 +0800

    调参

--- clDGR/mnist/wgan_gp.py
+++ clDGR/mnist/wgan_gp.py
@@ -12,7 +12,7 @@ class D(object):
                 vs.reuse_variables()
             conv1 = tf.layers.conv2d(x, 128, [4, 4], [2, 2], activation=tf.nn.leaky_relu)
             conv2 = tf.layers.conv2d(conv1, 64, [4, 4], [2, 2], activation=tf.nn.leaky_relu)
-            fc1 = tf.layers.dense(tf.layers.flatten(conv2), 1024, activation=tf.nn.leaky_relu)
+            fc1 = tf.layers.dense(tf.layers.flatten(conv2), 512, activation=tf.nn.leaky_relu)
             fc2 = tf.layers.dense(fc1, 1)
             return fc2
 
@@ -23,7 +23,7 @@ class D(object):
 
 class G(object):
     def __init__(self):
-        self.z_dim = 100
+        self.z_dim = 64
         self.imshape = [28, 28, 1]
         self.name = 'mnist/g_net'
 
@@ -32,14 +32,15 @@ class G(object):
             bs = tf.shape(z)[0]
             fc1 = tf.layers.dense(z, 7 * 7 * 128, activation=tf.nn.relu)
             conv1 = tf.reshape(fc1, [bs, 7, 7, 128])
-            conv1 = tf.layers.conv2d_transpose(conv1, 128, [4, 4], [2, 2],
+            conv1 = tf.layers.conv2d_transpose(conv1, 128, [4, 4], [2, 2], 'SAME',
                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(2.5e-5),
                                                activation=tf.nn.relu)
-            conv2 = tf.layers.conv2d_transpose(conv1, 32, [4, 4], [2, 2],
+            conv2 = tf.layers.conv2d_transpose(conv1, 32, [4, 4], [2, 2], 'SAME',
                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(2.5e-5),
                                                activation=tf.nn.relu)
-            fc2 = tf.layers.dense(tf.layers.flatten(conv2), 784, activation=tf.nn.relu)
-            fake = tf.reshape(fc2, [bs] + self.imshape)
+            fake = tf.layers.conv2d_transpose(conv2, 1, [4, 4], [1, 1], 'SAME',
+                                              kernel_regularizer=tf.contrib.layers.l2_regularizer(2.5e-5),
+                                              activation=tf.nn.sigmoid)
             return fake
 
     @property

6ebf36272e3929939d2317b22b6fbb56668b4d6b
yx <yxue3357@163.com>
2018-04-21 08:59:23 +0800

    refactor

---
+++ GANs/SupervisedMaskGAN/plott.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def imcombind_(images, width=6):
+    num = images.shape[0]
+    height = int(math.ceil(float(num) / width))
+    shape = images.shape[1:4]
+    image = np.zeros((height * shape[0], width * shape[1], shape[2]), dtype=images.dtype)
+    for index, img in enumerate(images):
+        i = int(index / width)
+        j = index % width
+        image[i * shape[0]:(i + 1) * shape[0], j * shape[1]:(j + 1) * shape[1]] = img[:, :, :]
+    return image
+
+
+def imsave_(path, img):
+    plt.imsave(path, np.squeeze(img))

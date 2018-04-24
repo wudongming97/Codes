599f88ca15102e639aa96c6f81b2144d0a83ae53
yx <yxue3357@163.com>
2018-04-24 16:11:31 +0800

    clean

--- GANs/Pytorch-CycleGAN/networks.py
+++ GANs/Pytorch-CycleGAN/networks.py
@@ -133,24 +133,3 @@ class NLayerDiscriminator(nn.Module):
 
     def forward(self, input):
         return nn.parallel.data_parallel(self.model, input)
-
-
-class PixelDiscriminator(nn.Module):
-    def __init__(self, input_nc, ndf=64, norm_layer=nn.InstanceNorm2d, use_sigmoid=False):
-        super(PixelDiscriminator, self).__init__()
-
-        self.net = [
-            nn.Conv2d(input_nc, ndf, kernel_size=1, stride=1, padding=0),
-            nn.LeakyReLU(0.2, True),
-            nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=True),
-            norm_layer(ndf * 2),
-            nn.LeakyReLU(0.2, True),
-            nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=True)]
-
-        if use_sigmoid:
-            self.net.append(nn.Sigmoid())
-
-        self.net = nn.Sequential(*self.net)
-
-    def forward(self, input):
-        return nn.parallel.data_parallel(self.net, input)

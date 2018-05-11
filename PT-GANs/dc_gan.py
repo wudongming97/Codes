8ea98d35da6429df0c05a6c82a0e74b3f6433410
yx <yxue3357@163.com>
2018-05-11 22:38:50 +0800

    refine

--- PT-GANs/dc_gan.py
+++ PT-GANs/dc_gan.py
@@ -26,7 +26,7 @@ batch_size = 128
 _transformer = tv.transforms.Compose([
     tv.transforms.Resize(im_size),
     tv.transforms.ToTensor(),
-    tv.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    tv.transforms.Normalize([0.5] * 3, [0.5] * 3)
 ])
 
 train_iter = T.utils.data.DataLoader(
@@ -42,11 +42,24 @@ train_iter = T.utils.data.DataLoader(
 )
 
 
+# train_iter = T.utils.data.DataLoader(
+#     dataset=tv.datasets.CIFAR100(
+#         root='../../Datasets/CIFAR100/',
+#         transform=_transformer,
+#         download=True,
+#         train=True,
+#     ),
+#     batch_size=batch_size,
+#     shuffle=True,
+#     drop_last=True,
+#     num_workers=2,
+# )
+
+
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
-        m.bias.data.fill_(0)
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
@@ -61,23 +74,23 @@ class generator(nn.Module):
         super(generator, self).__init__()
         self.net = nn.Sequential(
             # layer 1
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0),
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.Dropout(0.2),
             nn.ReLU(True),
             # layer 2 (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # layer 3
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # layer 4
-            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # layer 5
-            nn.ConvTranspose2d(ngf, nc, 4, 2, 1),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
         )
 
@@ -90,39 +103,39 @@ class discriminator(nn.Module):
         super(discriminator, self).__init__()
         self.net = nn.Sequential(
             # layer 1
-            nn.Conv2d(nc, ndf, 4, 2, 1),
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.Dropout(0.2),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, True),
             # layer 2
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, True),
             # layer 3
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, True),
             # layer 3
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, True),
             # layer 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0),
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        return self.net(x).view(-1, 1).squeeze(1)
+        return nn.parallel.data_parallel(self.net, x).view(-1, 1).squeeze(1)
 
 
-G = generator(nz, 3, 128).apply(weights_init).to(DEVICE)
-D = discriminator(3, 128).apply(weights_init).to(DEVICE)
+G = generator(nz, 3, 64).apply(weights_init).to(DEVICE)
+D = discriminator(3, 64).apply(weights_init).to(DEVICE)
 
 print('网络结构!!' + '\n' + '--' * 30)
 print(G)
 print(D)
 
-opt_G = optim.SGD(G.parameters(), lr)
-opt_D = optim.SGD(D.parameters(), lr)
+opt_G = optim.Adam(G.parameters(), lr, betas=[0.5, 0.999])
+opt_D = optim.Adam(D.parameters(), lr, betas=[0.5, 0.999])
 
 scheduler_lr = lr_scheduler.StepLR(opt_G, step_size=1, gamma=0.9)
 
@@ -141,7 +154,7 @@ for epoch in range(0, n_epochs):
     scheduler_lr.step()
     for X, _ in train_iter:
         _batch += 1
-        # G
+
         x_real = X.to(DEVICE)
         z = fixed_noise
         fake_x = G(z)
@@ -152,16 +165,16 @@ for epoch in range(0, n_epochs):
         r_label = T.full((batch_size,), real_label, device=DEVICE)
         f_label = T.full((batch_size,), fake_label, device=DEVICE)
 
-        lss_D = F.binary_cross_entropy(real_score, r_label) + F.binary_cross_entropy(fake_score, f_label)
         D.zero_grad()
+        lss_D = F.binary_cross_entropy(real_score, r_label) + F.binary_cross_entropy(fake_score, f_label)
         lss_D.backward()
         opt_D.step()
 
         fake_score = D(fake_x.detach())
         real_score = D(x_real)
 
-        lss_G = F.binary_cross_entropy(fake_score, r_label)
         G.zero_grad()
+        lss_G = F.binary_cross_entropy(fake_score, r_label)
         lss_G.backward()
         opt_G.step()
 
@@ -175,5 +188,5 @@ for epoch in range(0, n_epochs):
             tv.utils.save_image((fake_x.detach()[:64] + 1.) / 2., save_dir + '{}_{}.png'.format(epoch, _batch))
 
     if epoch % save_epoch_freq == 0:
-        T.save(D.state_dict(), 'G_{}.pt'.format(epoch))
-        T.save(D.state_dict(), 'D_{}.pt'.format(epoch))
+        T.save(D.state_dict(), 'dcgan_netd.pth')
+        T.save(G.state_dict(), 'dcgan_netg.pth')

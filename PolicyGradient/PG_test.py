0d3f271f84254a32ec202bff1253079f13fb6044
yx <yxue3357@163.com>
2018-08-05 19:08:00 +0800

    a2c

--- PolicyGradient/PG_test.py
+++ PolicyGradient/PG_test.py
@@ -5,7 +5,7 @@ N_EPISODES = 2
 from models import *
 from utils import *
 
-model_name = 'PG_NaiveAC'
+model_name = 'PG_A2C_01'
 # model_name = 'ReinforceNaive_01'
 env_id = "CartPole-v0"
 identity = env_id + '_' + model_name
@@ -14,13 +14,5 @@ net = NaiveAC(env.observation_space.shape[0], env.action_space.n)
 net.load_state_dict(torch.load(identity + '.pth'))
 
 for i_episode in range(N_EPISODES):
-    state = env.reset()
-    env.render()
-    total_reward = 0
-    is_done = False
-    while not is_done:
-        action, _, _, _ = net.action_and_logprob(state)
-        state, reward, is_done, _ = env.step(action)
-        total_reward += reward
-        env.render()
+    total_reward = test_policy(net, env, True)
     print('Episode: %d total_reward: %.3f' % (i_episode, total_reward))

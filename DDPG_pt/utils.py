e83f21c38bf10a8901c592a42cc9ca72d087639e
yx <yxue3357@163.com>
2018-08-08 09:50:47 +0800

    add test

--- DDPG_pt/utils.py
+++ DDPG_pt/utils.py
@@ -21,3 +21,20 @@ def soft_update(target, source, tau):
 def hard_update(target, source):
     for target_param, param in zip(target.parameters(), source.parameters()):
         target_param.data.copy_(param.data)
+
+
+def test_policy(actor, env, vis=False, n_episodes=2, max_len=500):
+    returns = []
+    for i_episode in range(n_episodes):
+        state = env.reset()
+        if vis: env.render()
+        episode_return = 0
+        for t in range(max_len):
+            action = actor.action(state)
+            state, reward, done, _ = env.step(action)
+            episode_return += reward
+            if vis: env.render()
+            if done:
+                returns.append(episode_return)
+                break
+    return sum(returns) / len(returns)

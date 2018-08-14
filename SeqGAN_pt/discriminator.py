51e1e70d4cd8f916be98c89eb775d95b10892569
yx <yxue3357@163.com>
2018-08-14 10:38:51 +0800

    clean

--- SeqGAN_pt/discriminator.py
+++ SeqGAN_pt/discriminator.py
@@ -5,33 +5,6 @@ import torch.nn.functional as F
 from utils import DEVICE
 
 
-class RNNDiscriminator(nn.Module):
-    def __init__(self, voc_size, emb_size, hid_size, max_seq_len=20, dropout_prob=0.2):
-        super(RNNDiscriminator, self).__init__()
-        self.max_seq_len = max_seq_len
-        self.hid_size = hid_size
-        self.embeddings = nn.Embedding(voc_size, emb_size)
-        self.gru = nn.GRU(emb_size, hid_size, batch_first=True)
-        self.fc_out = nn.Sequential(
-            nn.Linear(hid_size, 1),
-            nn.Dropout(p=dropout_prob),
-            nn.Sigmoid()
-        )
-        self.to(DEVICE)
-
-    def forward(self, x):
-        emb = self.embeddings(x)
-        hid = self.gru(emb)[1].view(-1, self.hid_size)
-        return self.fc_out(hid)
-
-    def rewards(self, rolls):
-        rets = []
-        for r in rolls:
-            ret_t = self.forward(r)
-            rets.append(ret_t)
-        return torch.cat(rets, 1)
-
-
 # borrow from https://github.com/ZiJianZhao/SeqGAN-PyTorch/blob/master/discriminator.py
 class CNNDiscriminator(nn.Module):
     def __init__(self, voc_size, emb_size=64, dropout_prob=0.5,

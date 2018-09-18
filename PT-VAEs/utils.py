ad1d51c9182144bc0f9dc456b503f6e30851f3d4
yx <yxue3357@163.com>
2018-09-18 10:08:17 +0800

    refactor

--- PT-VAEs/utils.py
+++ PT-VAEs/utils.py
@@ -1,11 +1,8 @@
-import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.utils.data as data
 import torchvision as tv
-from PIL import Image
 
 _use_cuda = torch.cuda.is_available()
 DEVICE = torch.device('cuda' if _use_cuda else 'cpu')
@@ -33,35 +30,9 @@ def plot_q_z(x, y, filename):
     plt.close()
 
 
-def _file_paths(dir, ext):
-    paths = []
-    for entry in os.scandir(dir):
-        if entry.is_file() and entry.path.endswith(ext):
-            paths.append(entry.path)
-        elif entry.is_dir():
-            paths.extend(_file_paths(entry.path, ext))
-    return paths
-
-
-class MonolingualImageFolder(data.Dataset):
-    def __init__(self, root, ext='.png', transform=None):
-        self.img_paths = _file_paths(root, ext)
-        self.L = len(self.img_paths)
-        self.transform = transform
-
-    def __len__(self):
-        return self.L
-
-    def __getitem__(self, item):
-        img = Image.open(self.img_paths[item])
-        if self.transform is not None:
-            img = self.transform(img)
-        return img
-
-
 def anime_face_loader(root, transform, batch_size=128):
     face_loader = torch.utils.data.DataLoader(
-        dataset=MonolingualImageFolder(root=root, transform=transform),
+        dataset=tv.datasets.ImageFolder(root=root, transform=transform),
         batch_size=batch_size, shuffle=True)
     return face_loader
 
